c4dd436eb2873de6da7bc177d23e05c395dd3f0a
ananas <lucienfiorini@protonmail.com>
2020-06-04 14:06:08 +0100

    Add main file

---
+++ main.py
@@ -0,0 +1,16 @@
+#!/usr/env python
+
+import RPI.GPIO as GPIO
+from time import sleep
+
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(7, GPIO.OUT)
+
+for x in range(0, 3):
+    GPIO.output(7, True)
+    sleep(1)
+    GPIO.output(7, False)
+    sleep(1)
+
+# Clean the GPIO
+GPIO.cleanup()
