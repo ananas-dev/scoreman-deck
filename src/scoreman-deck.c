6a1711225c44c2c3ebcc7a65301b0adc702d2752
ananas <lucien@workstation.localdomain>
2020-06-07 22:10:07 +0200

    Tests with the wiringPi lib

--- src/scoreman-deck.c
+++ src/scoreman-deck.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-#include "include/bcm2835.h"
+#include "../include/bcm2835.h"
 
 /* Map pins */
-#define LED 17
+#define BUTTON1 18
+#define BUTTON2 23
+#define BUTTON3 24
+#define BUTTON4 25
 
 int main(void)
 {
@@ -11,16 +14,29 @@ int main(void)
     /* Initialize bcm2835 */
     if (!bcm2835_init()) return 1;
 
-    bcm2835_gpio_fsel(LED, BCM2835_GPIO_FSEL_OUTP);
-    unsigned int delay = 1000;
+    /* Set buttons to output mode */
+    bcm2835_gpio_fsel(BUTTON1, BCM2835_GPIO_FSEL_INPT);
+    bcm2835_gpio_fsel(BUTTON2, BCM2835_GPIO_FSEL_INPT);
+    bcm2835_gpio_fsel(BUTTON3, BCM2835_GPIO_FSEL_INPT);
+    bcm2835_gpio_fsel(BUTTON4, BCM2835_GPIO_FSEL_INPT);
+    /* Set pud-up */
+    bcm2835_gpio_set_pud(BUTTON1, BCM2835_GPIO_PUD_UP);
+    bcm2835_gpio_set_pud(BUTTON2, BCM2835_GPIO_PUD_UP);
+    bcm2835_gpio_set_pud(BUTTON3, BCM2835_GPIO_PUD_UP);
+    bcm2835_gpio_set_pud(BUTTON4, BCM2835_GPIO_PUD_UP);
+    
+    unsigned int delay = 50;
 
-    int x;
-    for (x=0; x<5; x++)
+/*
+    while(1)
     {
-        bcm2835_gpio_set(LED);
+        bcm2835_gpio_write(LED, LOW);
         bcm2835_delay(delay);
-        bcm2835_gpio_clr(LED);
+        bcm2835_gpio_write(LED, HIGH);
         bcm2835_delay(delay);
     }
+    bcm2835_close();
     return 0;
 }
+
+*/
