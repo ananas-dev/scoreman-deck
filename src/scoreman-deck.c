7c37392db9c6ac4e9961c943689482084e03a5af
ananas <lucien@workstation.localdomain>
2020-06-07 23:45:30 +0200

    Support for 4 buttons

--- src/scoreman-deck.c
+++ src/scoreman-deck.c
@@ -1,11 +1,16 @@
 #include <wiringPi.h>
 #include<stdio.h>
 
-#define buttonPin 1
+/* Pins layout */
+#define BUTTON_1 1 // Pin 18
+#define BUTTON_2 4 // Pin 23
+#define BUTTON_3 5 // Pin 24
+#define BUTTON_4 6 // Pin 25
 
 int main(void)
 {
     short input;
+    short lastInput;
 
     printf(":: Starting...\n");
 
@@ -14,20 +19,47 @@ int main(void)
     wiringPiSetup(); 
 
     /* Set the button to input */
-    printf(" (2/3) Set the button to input\n");
-    pinMode(buttonPin, INPUT);
+    printf(" (2/3) Set the buttons to input\n");
+    pinMode(BUTTON_1, INPUT);
+    pinMode(BUTTON_2, INPUT);
+    pinMode(BUTTON_3, INPUT);
+    pinMode(BUTTON_4, INPUT);
 
     /* Set pull up to HIGH level */
     printf(" (3/3) Set pull up to HIGH level\n");
-    pullUpDnControl(buttonPin, PUD_UP);
+    pullUpDnControl(BUTTON_1, PUD_UP);
+    pullUpDnControl(BUTTON_2, PUD_UP);
+    pullUpDnControl(BUTTON_3, PUD_UP);
+    pullUpDnControl(BUTTON_4, PUD_UP);
 
     /* Main loop */
+
+    printf(":: Listenig inputs")
     while(1)
     {
-        if (digitalRead(buttonPin) == LOW)
+        /* Check if the buttons are pushed */
+        if (digitalRead(BUTTON_1) == LOW)
         {
             input = 0x1;
         }
+        if (digitalRead(BUTTON_1) == LOW)
+        {
+            input = 0x2;
+        }
+        if (digitalRead(BUTTON_1) == LOW)
+        {
+            input = 0x3;
+        }
+        if (digitalRead(BUTTON_1) == LOW)
+        {
+            input = 0x4;
+        }
+        /* Only output each inputs once */
+        if(input != lastInput)
+        {
+            printf("%x\n", input);
+        }
+        lastInput = input;
     }
     return 0;
 }
